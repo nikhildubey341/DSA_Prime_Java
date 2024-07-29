a0b0059b380e8c3c5b45b15befda11e53a0906cd
Nikhil Dubey <nikhildubey714@gmail.com>
2024-07-29 21:35:27 +0530

    Module 1 - 1.3(Java Conditional Statements -->If-else & Switch)

---
+++ src/Module_1_IntroductionToJava/C_Conditional_Statements/b_LearnSwitch.java
@@ -0,0 +1,46 @@
+package Module_1_IntroductionToJava.C_Conditional_Statements;
+
+import java.util.Scanner;
+
+public class b_LearnSwitch {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the day");
+        int day = sc.nextInt();
+
+//        if(day == 1) {
+//            System.out.println("Today is Sunday");
+//        } else if(day == 2) {
+//            System.out.println("Today is Sunday");
+//        }
+
+        switch (day) {
+            case 1:
+                System.out.println("Today is Sunday");
+                System.out.println("this is on sunday");
+                break;
+            case 2:
+                System.out.println("Today is Monday");
+                break;
+            case 3:
+                System.out.println("Today is Tuesday");
+                break;
+            case 4:
+                System.out.println("Today is Wednesday");
+                break;
+            case 5:
+                System.out.println("Today is Thursday");
+                break;
+            case 6:
+                System.out.println("Today is Friday");
+                break;
+            case 7:
+                System.out.println("Today is Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+
+    }
+}
